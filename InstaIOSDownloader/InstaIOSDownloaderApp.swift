803996097d28b2caa1fa57b00fbe12f06ef445a4
Neawor Taha <neawortaha@MacBookAir.attlocal.net>
2025-09-18 20:09:09 -0500

    initial commit

---
+++ InstaIOSDownloader/InstaIOSDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaIOSDownloaderApp.swift
+//  InstaIOSDownloader
+//
+//  Created by Neawor Taha on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct InstaIOSDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstaIOSDownloaderTests/InstaIOSDownloaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  InstaIOSDownloaderTests.swift
+//  InstaIOSDownloaderTests
+//
+//  Created by Neawor Taha on 9/18/25.
+//
+
+import Testing
+
+struct InstaIOSDownloaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
